1a0a162a675394ec4423070cd48d6f98b22a9ada
movarnell <michaelvarnell@icloud.com>
2023-04-08 09:49:21 -0500

    cards displaying, but not centered

--- src/Components/StoreFront.js
+++ src/Components/StoreFront.js
@@ -1,31 +1,28 @@
-import {useState, useEffect} from 'react';
-import { API } from '../API/Api';
-import ProductCards from './ProductCards'
-import '../../node_modules/bootstrap/dist/css/bootstrap.css'
+import { useState, useEffect } from "react";
+import { API } from "../API/Api";
+import ProductCards from "./ProductCards";
+import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
+export default function StoreFront() {
+	const [products, setProducts] = useState([]);
 
-export default function StoreFront(){
-const [products, setProducts] = useState([]);
+	useEffect(() => {
+		fetchProducts();
+	}, []);
 
+	const fetchProducts = async () => {
+		const addProduct = await API.getProducts();
+		console.log(addProduct);
+		setProducts(addProduct);
+		console.log(addProduct);
+	};
 
-
-useEffect(() => {
-    fetchProducts();
-}, []);
-
-
-
-const fetchProducts = async () => {const addProduct = await API.getProducts();
-    console.log(addProduct);
-    setProducts(addProduct);
-    console.log(addProduct);
-};
-
-console.log(products);
-return(
-    <div className='container'>
-        <ProductCards products={products} />
-    </div>
-)
-
+	console.log(products);
+	return (
+		<div className="container-fluid cardContainer">
+			<div className="row">
+				<ProductCards products={products} />
+			</div>
+		</div>
+	);
 }
